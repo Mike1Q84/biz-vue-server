89e2370cfed5da17439173dfac51c91f6ffd64e9
Mike1Q84 <mike.zhang.monash@gmail.com>
2018-09-14 16:06:37 +1000

    chore: Remove unused imports

--- webpack-dev.config.ts
+++ webpack-dev.config.ts
@@ -1,6 +1,5 @@
 import { Configuration } from 'webpack';
 import merge from 'webpack-merge';
-import webpackNodeExternals from 'webpack-node-externals';
 import commonConfig from './webpack.config';
 
 export const config: Configuration = merge(commonConfig, {
