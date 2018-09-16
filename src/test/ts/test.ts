da6aa98f602373dfb1531fb06436664bdaefd248
Mike1Q84 <mike.zhang.monash@gmail.com>
2018-09-16 19:45:09 +1000

    test: Add basic health controller test

---
+++ src/test/ts/test.ts
@@ -0,0 +1,10 @@
+import { Container, ContainerInstance } from 'typedi';
+
+import { ServerDiModule } from '~dimodule';
+
+export const getContainer = () => {
+	const ci: ContainerInstance = Container.of(undefined);
+	new ServerDiModule(ci);
+
+	return ci;
+};
