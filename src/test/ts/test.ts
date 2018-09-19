3e7818f46275aabead50486513b6790c66aadb47
Mike1Q84 <mike.zhang.monash@gmail.com>
2018-09-19 18:09:27 +1000

    chore: Configure test helper to connect to test MongoDB for local test environment

--- src/test/ts/test.ts
+++ src/test/ts/test.ts
@@ -1,3 +1,4 @@
+import mongoose from 'mongoose';
 import 'reflect-metadata';
 import { Container, ContainerInstance } from 'typedi';
 
@@ -9,3 +10,17 @@ export const getContainer = () => {
 
 	return ci;
 };
+
+// MongoDB test helper
+mongoose.set('useNewUrlParser', true);
+mongoose.set('useFindAndModify', false);
+mongoose.set('useCreateIndex', true);
+
+before(async () => {
+	const ci = getContainer();
+	const mongoUri = ci.get('mongo');
+	mongoose.connect(`${mongoUri}_test`);
+	mongoose.connection
+		.once('open', () => console.log('MongoDB successfully connected.'))
+		.on('error', err => console.error(err));
+});
